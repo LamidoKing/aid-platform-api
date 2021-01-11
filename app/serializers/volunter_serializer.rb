f025cf6f9ea34a2b8527970a298cc71730c72352
lamido tijjani <kinglamido@gmail.com>
2021-01-11 01:40:32 +0100

    ch:edit request and volunter serialize

--- app/serializers/volunter_serializer.rb
+++ app/serializers/volunter_serializer.rb
@@ -1,4 +1,8 @@
 # Volunters Serializer
 class VolunterSerializer < ActiveModel::Serializer
   attributes :id, :user, :request, :created_at, :updated_at
+
+  def user
+    object.user.attributes.except('password_digest', 'created_at', 'updated_at')
+  end
 end
