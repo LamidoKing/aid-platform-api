3143478b593b5b5ff44d2db240544f1c2a1fc3af
Lamido <34864499+LamidoKing@users.noreply.github.com>
2021-01-22 04:24:57 +0100

    Revert "ch: fix actionstorage validation issue"

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,7 @@ class User < ApplicationRecord
   has_many :messages, class_name: 'Message', foreign_key: 'sender', dependent: :destroy
   has_many :messages, class_name: 'Message', foreign_key: 'receiver', dependent: :destroy
   has_many :volunters, dependent: :destroy
-  validates :govnt_id,
-            content_type: { in: ['image/png', 'image/jpg', 'image/jpeg', 'application/pdf'], message: 'Id must be png jpg or pdf' },
-            size: { less_than: 5.megabytes, message: 'Id must be lessthan 5mb' },
-            on: :create
+  validates :govnt_id, presence: true, blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg', 'application/pdf'] } unless :govnt_id.present?
   validates :first_name, :last_name, presence: true
   EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, format: { with: EMAIL_FORMAT }, uniqueness: true
